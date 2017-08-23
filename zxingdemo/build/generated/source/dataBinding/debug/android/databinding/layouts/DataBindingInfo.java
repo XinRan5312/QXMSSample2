cdd13aa81591421e20879f244ae231fa3239f4fe
houqixin <tiemaocsdn@qq.com>
2017-08-23 15:25:20 +0800

    添加ZXing和zbar二维码相关demo 很全的偶 感谢大神王浩

--- zxingdemo/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="5eee079a-afb2-4cca-9b8e-63c5d25dcc9e")
-public class DataBindingInfo {}
